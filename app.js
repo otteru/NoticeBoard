d8497635b67a08f0c03fe5fb007f4da457b1ee7f
otteru <otteru@naver.com>
2024-02-13 08:55:20 +0000

    make login page & bring static file(css,js)

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+const path = require("path");
+// path는 따로 패키지가 아니라 node.js에 기본적으로 있는 거다.
+
 const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+// 정적인 css, js 파일 불러오는 것을 위한 코드
+//(public 폴더에 정적인 파일들이 있음)
+  
 //app.set("vies", path.join(__dirname, "views"));
 //app.set("view engine", "ejs");
 
 app.get("/", function(req,res) {
-	res.send("<h1>hello</h1>");
+	const htmlFilePath = path.join(__dirname, "views", "logIn.html");
+	res.sendFile(htmlFilePath);
 })
-
+ 
 app.get("/home", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "home.html");
-	res.sendFile("home.html");
-})
-app.listen(3000); 
+	res.sendFile(htmlFilePath);
+});
+app.listen(3000);   
