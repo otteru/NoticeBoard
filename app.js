7e72fc57e0b84e22217a27c031bdef467949a338
otteru <otteru@naver.com>
2024-02-16 02:28:36 +0000

    adjust ejs

--- app.js
+++ app.js
@@ -7,18 +7,26 @@ const express = require("express");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//ejs 사용을 위한 setting
+
 app.use(express.static("public"));
 // 정적인 css, js 파일 불러오는 것을 위한 코드
 //(public 폴더에 정적인 파일들이 있음)
 app.use(express.urlencoded({ extended: false }));
 // POST 요청의 본문 해석하기 위한 미들웨어
-  
-//app.set("vies", path.join(__dirname, "views"));
-//app.set("view engine", "ejs");
 
 app.get("/", function(req,res) {
-	const htmlFilePath = path.join(__dirname, "views", "signIn.html");
-	res.sendFile(htmlFilePath);
+	// const htmlFilePath = path.join(__dirname, "views", "signIn.html");
+	// res.sendFile(htmlFilePath); --> ejs를 사용하지 않았을 때의 코드
+	
+	const filePath = path.join(__dirname, "data", "users.json");
+	
+	const fileData = fs.readFileSync(filePath);
+	const storedUsers = JSON.parse(fileData);
+	
+	res.render("signIn", {numberOfUsers: storedUsers.length});
 });
 
 app.post("/", function(req, res) {
@@ -51,8 +59,7 @@ app.post("/", function(req, res) {
 })
 
 app.get("/signUp", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "signUp.html");
-	res.sendFile(htmlFilePath);
+	res.render("signUp");
 });
 
 
@@ -94,8 +101,12 @@ app.post("/signUp", function(req, res){
 });
 
 app.get("/home", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "home.html");
-	res.sendFile(htmlFilePath);
+	res.render("home");
 });
+
+app.get("/write", function(req, res) {
+	res.render("write");
+});
+
 
 app.listen(3000);  
